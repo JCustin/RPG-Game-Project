343f08de6d93c656e04f74d28ab85d0e0f45e63e
Justin Cervantes <jus.cervantes99@gmail.com>
2026-04-20 20:37:06 -0700

    Refined the combat_scene_logic script a bit more.

--- Combat_Scene_Test/combat_scene_logic.gd
+++ Combat_Scene_Test/combat_scene_logic.gd
@@ -39,19 +39,11 @@ func _ready() -> void:
 	active_players_in_combat += [primary_enemy]
 	
 	turn_queue = %Timeline_System.assign_turn_queue(active_players_in_combat)
+	active_players_in_combat = turn_queue
 	
 	spawn_and_position_actors()
 	acting_player = turn_queue[0]
-	print_debug(turn_queue)
-	active_players_in_combat = turn_queue
-	#print_debug("the acting player is: ", acting_player)
-	
-	if acting_player == primary_enemy:
-		start_enemy_turn()
-	else:
-		start_player_turn()
-
-	#target_enemy = potential_enemy_targets[0]
+	start_combat_round()
 	
 func spawn_and_position_actors():
 	#acting_player.position = Vector2(200, -50)
@@ -60,7 +52,6 @@ func spawn_and_position_actors():
 	pass
 	
 func _on_attack_pressed() -> void:
-	
 	potential_enemy_targets = enemy_logic.return_active_enemy_parts(forward_combat_direction, primary_enemy)
 	target_enemy = potential_enemy_targets[0]
 	player_logic.highlight_enemy(target_enemy)
@@ -72,7 +63,6 @@ func _on_attack_pressed() -> void:
 	player_choosing_target = true
 	
 func _input(event: InputEvent) -> void:
-	
 	if player_choosing_target == true:
 		if Input.is_action_just_pressed("ui_left"):
 			if (potential_enemy_target_index - 1) < 0:
@@ -107,7 +97,6 @@ func _input(event: InputEvent) -> void:
 
 func execute_attack(target: Node2D, attack_value: int, attack_description: String):
 	await prompt_combat_description(attack_description)
-	#print_debug(target, attack_value)
 	
 	if target == primary_enemy:
 		pass
@@ -116,38 +105,12 @@ func execute_attack(target: Node2D, attack_value: int, attack_description: Strin
 		pass
 		
 	proceed_with_turn_queue()
-	
-	
-	
-	#if %Player_Actors.get_children().has(target): # in other words, if the target is a PLAYER
-		#pass
-	#
-	#else: # in other words, if the target is an ENEMY
-		#print_debug("OK the target is an enemy")
-		##continue_or_end_player_turn()
-		
-
-#func continue_or_end_player_turn():
-	#var player_index = turn_queue.find(acting_player)
-	#if players_acted_this_turn.has(acting_player):
-		#if players_acted_this_turn.size() > (player_index + 1):
-			#acting_player = players_acted_this_turn[player_index + 1]
-			#
-			#for button in %GUI.get_children():
-				#button.disabled = false
-				#button.visible = false
-				#
-		#elif players_acted_this_turn.size() <= (player_index + 1):
-			#enemy_logic.start_enemy_turn(primary_enemy, turn_queue)
-			#pass
-	#pass
 
 func prompt_combat_description(attack_description: String) -> void:
 	var combat_label = Label.new()
 	%Combat_Description_Panel.add_child(combat_label)
 	combat_label.anchor_left
 	combat_label.text = attack_description
-	
 	
 	%Combat_Description_Master.visible = true
 	
@@ -163,19 +126,14 @@ func start_enemy_turn():
 		button.visible = false
 	
 func start_player_turn():
-	print_debug("Should be my turn now")
-	print_debug(acting_player)
 	for button in %GUI.get_children():
 		button.disabled = false
 		button.visible = true
-		
-		
-	
+
 func end_combat():
 	pass
 
 func start_combat_round():
-	#acting_player = turn_queue[0]
 	if acting_player == primary_enemy:
 		start_enemy_turn()
 	else:
@@ -185,8 +143,6 @@ func start_combat_round():
 func proceed_with_turn_queue():
 	var current_turn_queue_index = turn_queue.find(acting_player)
 	var next_turn_queue_index: int = (current_turn_queue_index + 1)
-	
-	print_debug(current_turn_queue_index, next_turn_queue_index)
 	
 	if turn_queue.size() <= (next_turn_queue_index):
 		turn_queue = %Timeline_System.refresh_turn_queue(active_players_in_combat)
