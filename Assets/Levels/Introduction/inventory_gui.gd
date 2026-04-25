4c54c84a6c246773776f3c5cf582ed6c9c674f25
Justin <jus.cervantes99@gmail.com>
2026-04-25 08:55:12 -0700

    Added inventory menu to select items that are picked up AND I overhauled code for detecting and picking up items to work consistently.

---
+++ Assets/Levels/Introduction/inventory_gui.gd
@@ -0,0 +1,14 @@
+extends Control
+@export var inventory_list : ItemList
+
+
+func _ready() -> void:
+	for player in get_tree().get_nodes_in_group('Player'):
+		player.picked_up_item.connect(add_item_to_list)
+	
+func add_item_to_list(item: StaticBody2D) -> void:
+	inventory_list.add_item(item.item_name, null, true)
+
+
+func _on_inventory_list_item_selected(index: int) -> void:
+	print_debug(index)
--- Player Characters/Kevin/Script/kevin_player_overworld.gd
+++ Player Characters/Kevin/Script/kevin_player_overworld.gd
@@ -16,33 +16,57 @@ func _ready() -> void:
 	add_to_group('Player')
 
 func _physics_process(delta: float) -> void:	
+		
+		
 		move_and_slide()
 		if get_slide_collision_count() > 0:
 			send_collision_data()
 			
-func _input(event: InputEvent) -> void:
-		if active == true:
-			var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-			velocity = direction * speed
 			
-			if direction == Vector2.LEFT:
-				%Object_Raycast_Detection.rotation_degrees = 90
-			if direction == Vector2.RIGHT:
-				%Object_Raycast_Detection.rotation_degrees = -90
-			if direction == Vector2.UP:
-				%Object_Raycast_Detection.rotation_degrees = 180
-			if direction == Vector2.DOWN:
-				%Object_Raycast_Detection.rotation_degrees = 0
+			
+func _input(event: InputEvent) -> void:
+	if active == true:
+		var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+		velocity = direction * speed
 		
-		if Input.is_action_pressed("ui_accept"):
-			var object : Variant = %Object_Raycast_Detection.get_collider()
-
-			if object.is_class("StaticBody2D"):
-				interact_with_object(object)
-			elif object.is_class("TileMapLayer"):
-				interact_with_terrain(object)
-			else:
-				pass
+		if Input.is_action_just_pressed("ui_left"):
+			%Object_Raycast_Detection.rotation_degrees = 90
+		if Input.is_action_just_pressed("ui_right"):
+			%Object_Raycast_Detection.rotation_degrees = -90
+		if Input.is_action_just_pressed("ui_up"):
+			%Object_Raycast_Detection.rotation_degrees = 180
+		if Input.is_action_just_pressed("ui_down"):
+			%Object_Raycast_Detection.rotation_degrees = 0
+			
+		if Input.is_action_just_pressed("ui_accept"):
+			if %Object_Raycast_Detection.is_colliding() == true:
+				
+				var object : Variant = %Object_Raycast_Detection.get_collider()
+				if object.is_class("StaticBody2D"):
+					interact_with_object(object)
+				elif object.is_class("TileMapLayer"):
+					interact_with_terrain(object)
+				else:
+					pass
+				
+#func _input(event: InputEvent) -> void:
+		#if active == true:
+			#var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+			#velocity = direction * speed
+			#
+			#if direction == Vector2.LEFT:
+				#%Object_Raycast_Detection.rotation_degrees = 90
+			#if direction == Vector2.RIGHT:
+				#%Object_Raycast_Detection.rotation_degrees = -90
+			#if direction == Vector2.UP:
+				#%Object_Raycast_Detection.rotation_degrees = 180
+			#if direction == Vector2.DOWN:
+				#%Object_Raycast_Detection.rotation_degrees = 0
+		#
+		#if Input.is_action_pressed("ui_accept"):
+			#var object : Variant = %Object_Raycast_Detection.get_collider()
+#
+			
 			
 
 
@@ -74,6 +98,7 @@ func interact_with_object(object: StaticBody2D) -> void:
 		var object_name : String = object.item_name
 		prompt_overworld_event_description.emit("You have picked up " + object_name)
 		object.reparent(%Player_Inventory)
+		picked_up_item.emit(object)
 		object.visible = false
 		object.collision_layer = 1
 		object.position = Vector2(1000, 1000)
