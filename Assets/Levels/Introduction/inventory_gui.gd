f0041219be79b227dd1f0c5534d031d05921f192
Justin <jus.cervantes99@gmail.com>
2026-04-25 09:42:57 -0700

    Made core logic for adding items to the inventory gui.

--- Assets/Levels/Introduction/inventory_gui.gd
+++ Assets/Levels/Introduction/inventory_gui.gd
@@ -7,7 +7,8 @@ func _ready() -> void:
 		player.picked_up_item.connect(add_item_to_list)
 	
 func add_item_to_list(item: StaticBody2D) -> void:
-	inventory_list.add_item(item.item_name, null, true)
+	#inventory_list.add_item(item.item_name, null, true)
+	inventory_list.set_item_tooltip(inventory_list.add_item(item.item_name, null, true), item.item_description)
 
 
 func _on_inventory_list_item_selected(index: int) -> void:
